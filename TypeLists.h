e9273628c578312e569819734f0e06754664bc93
David <david@templatious.org>
2014-10-10 16:12:16 +0300

    adjusted TypeLists namespace changes

--- TypeLists.h
+++ TypeLists.h
@@ -24,127 +24,127 @@
 
 BOOST_AUTO_TEST_CASE( typelist_test_equality_0 )
 {
-    typedef tt::u::TypeList<void,int,char> AList;
-    typedef tt::u::TypeList<void,int,char> BList;
+    typedef tt::t::TypeList<void,int,char> AList;
+    typedef tt::t::TypeList<void,int,char> BList;
 
-    static const bool res = tt::u::TypelistsEqual<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistsEqual<AList,BList>::value;
     BOOST_CHECK(res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_equality_1 )
 {
-    typedef tt::u::TypeList<long> AList;
-    typedef tt::u::TypeList<long> BList;
+    typedef tt::t::TypeList<long> AList;
+    typedef tt::t::TypeList<long> BList;
 
-    static const bool res = tt::u::TypelistsEqual<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistsEqual<AList,BList>::value;
     BOOST_CHECK(res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_unequality_0 )
 {
-    typedef tt::u::TypeList<long,int> AList;
-    typedef tt::u::TypeList<long> BList;
+    typedef tt::t::TypeList<long,int> AList;
+    typedef tt::t::TypeList<long> BList;
 
-    static const bool res = tt::u::TypelistsEqual<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistsEqual<AList,BList>::value;
     BOOST_CHECK(!res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_unequality_1 )
 {
-    typedef tt::u::TypeList<long,int> AList;
-    typedef tt::u::TypeList<long,int,void> BList;
+    typedef tt::t::TypeList<long,int> AList;
+    typedef tt::t::TypeList<long,int,void> BList;
 
-    static const bool res = tt::u::TypelistsEqual<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistsEqual<AList,BList>::value;
     BOOST_CHECK(!res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_unequality_2 )
 {
-    typedef tt::u::TypeList<long,int,int> alist;
-    typedef tt::u::TypeList<long,int,void> blist;
+    typedef tt::t::TypeList<long,int,int> alist;
+    typedef tt::t::TypeList<long,int,void> blist;
 
-    static const bool res = tt::u::TypelistsEqual<alist,blist>::value;
+    static const bool res = tt::t::detail::TypelistsEqual<alist,blist>::value;
     BOOST_CHECK(!res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_unequality_3 )
 {
-    typedef tt::u::TypeList<int> AList;
-    typedef tt::u::TypeList<bool> BList;
+    typedef tt::t::TypeList<int> AList;
+    typedef tt::t::TypeList<bool> BList;
 
-    static const bool res = tt::u::TypelistsEqual<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistsEqual<AList,BList>::value;
     BOOST_CHECK(!res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_containment_0 )
 {
-    typedef tt::u::TypeList<int,int> AList;
-    typedef tt::u::TypeList<int> BList;
+    typedef tt::t::TypeList<int,int> AList;
+    typedef tt::t::TypeList<int> BList;
 
-    static const bool res = tt::u::TypelistContains<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistContains<AList,BList>::value;
     BOOST_CHECK(res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_containment_1 )
 {
-    typedef tt::u::TypeList<int,bool> AList;
-    typedef tt::u::TypeList<int,bool> BList;
+    typedef tt::t::TypeList<int,bool> AList;
+    typedef tt::t::TypeList<int,bool> BList;
 
-    static const bool res = tt::u::TypelistContains<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistContains<AList,BList>::value;
     BOOST_CHECK(res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_containment_2 )
 {
-    typedef tt::u::TypeList<int,bool,char,long,short> AList;
-    typedef tt::u::TypeList<int,bool,char> BList;
+    typedef tt::t::TypeList<int,bool,char,long,short> AList;
+    typedef tt::t::TypeList<int,bool,char> BList;
 
-    static const bool res = tt::u::TypelistContains<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistContains<AList,BList>::value;
     BOOST_CHECK(res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_uncontainment_0 )
 {
-    typedef tt::u::TypeList<bool,char,long,short> AList;
-    typedef tt::u::TypeList<int,bool,char> BList;
+    typedef tt::t::TypeList<bool,char,long,short> AList;
+    typedef tt::t::TypeList<int,bool,char> BList;
 
-    static const bool res = tt::u::TypelistContains<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistContains<AList,BList>::value;
     BOOST_CHECK(!res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_uncontainment_1 )
 {
-    typedef tt::u::TypeList<bool,char,long,short> AList;
-    typedef tt::u::TypeList<bool,char,long,short,int> BList;
+    typedef tt::t::TypeList<bool,char,long,short> AList;
+    typedef tt::t::TypeList<bool,char,long,short,int> BList;
 
-    static const bool res = tt::u::TypelistContains<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistContains<AList,BList>::value;
     BOOST_CHECK(!res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_uncontainment_2 )
 {
-    typedef tt::u::TypeList<bool> AList;
-    typedef tt::u::TypeList<int> BList;
+    typedef tt::t::TypeList<bool> AList;
+    typedef tt::t::TypeList<int> BList;
 
-    static const bool res = tt::u::TypelistContains<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistContains<AList,BList>::value;
     BOOST_CHECK(!res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_uncontainment_3 )
 {
-    typedef tt::u::TypeList<bool,int> AList;
-    typedef tt::u::TypeList<bool,void> BList;
+    typedef tt::t::TypeList<bool,int> AList;
+    typedef tt::t::TypeList<bool,void> BList;
 
-    static const bool res = tt::u::TypelistContains<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistContains<AList,BList>::value;
     BOOST_CHECK(!res);
 }
 
 BOOST_AUTO_TEST_CASE( typelist_test_uncontainment_4 )
 {
-    typedef tt::u::TypeList<bool,int,char,long,short> AList;
-    typedef tt::u::TypeList<int,char,long,short> BList;
+    typedef tt::t::TypeList<bool,int,char,long,short> AList;
+    typedef tt::t::TypeList<int,char,long,short> BList;
 
-    static const bool res = tt::u::TypelistContains<AList,BList>::value;
+    static const bool res = tt::t::detail::TypelistContains<AList,BList>::value;
     BOOST_CHECK(!res);
 }
 

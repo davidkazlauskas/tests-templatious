2d11cdcde3e3e4f719787305af8f5f87264d546d
David <david@templatious.org>
2014-10-13 20:43:58 +0300

    pack tests

---
+++ PackTests.h
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  PackTests.h
+ *
+ *    Description:  test for packs
+ *
+ *        Version:  1.0
+ *        Created:  10/13/2014 08:40:37 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#ifndef PACKTESTS_1UNIFFZN
+#define PACKTESTS_1UNIFFZN
+
+#include "TestDefs.h"
+
+BOOST_AUTO_TEST_SUITE( pack_tests )
+
+BOOST_AUTO_TEST_CASE( pack_basic_test )
+{
+
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif /* end of include guard: PACKTESTS_1UNIFFZN */
+

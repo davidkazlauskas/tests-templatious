097742d965162eceae5ba340c41fd626cef04d80
David <david@templatious.org>
2014-12-15 17:02:33 +0200

    new class carcass

---
+++ detail/ConstructorCountCollection.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ConstructorCountCollection.h
+ *
+ *    Description:  Collection that counts construction and destruction of objects
+ *
+ *        Version:  1.0
+ *        Created:  12/15/2014 05:01:24 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#ifndef CONSTRUCTORCOUNTCOLLECTION_APM2CV9I
+#define CONSTRUCTORCOUNTCOLLECTION_APM2CV9I
+
+namespace test_templatious {
+
+}
+
+#endif /* end of include guard: CONSTRUCTORCOUNTCOLLECTION_APM2CV9I */
+

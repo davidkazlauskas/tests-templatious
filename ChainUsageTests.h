a31e9ad3890a674c5fc32dee345f7ed901d52001
David <david@templatious.org>
2014-12-19 17:44:48 +0200

    block of solidity

--- ChainUsageTests.h
+++ ChainUsageTests.h
@@ -22,8 +22,100 @@
 
 #include "TestDefs.h"
 
-BOOST_AUTO_TEST_SUITE( chained_usage_test_suite );
+BOOST_AUTO_TEST_SUITE( chained_usage_tests );
 
+BOOST_AUTO_TEST_CASE( chained_usage_tests_varying_addition )
+{
+    INIT_BALLER;
+
+    std::vector<int> v;
+
+    { // pack, seq, vars
+        SA::clear(v);
+        int a,b,c,d,e,f,g;
+        auto p = SF::pack(a,b,c,d,e,f,g);
+        SM::distribute(SF::seqI(1,7),p);
+
+        SA::add(v,p,SF::seqI(17,27),a,b,c,d,e,f,g);
+
+        sum = 0;
+        SM::forEach(sf,v);
+
+        BOOST_CHECK( sum == 28 + 242 + 28 );
+    }
+
+    { // pack, vars, seq
+        SA::clear(v);
+        int a,b,c,d,e,f,g;
+        auto p = SF::pack(a,b,c,d,e,f,g);
+        SM::distribute(SF::seqI(1,7),p);
+
+        SA::add(v,p,a,b,c,d,e,f,g,SF::seqI(17,27));
+
+        sum = 0;
+        SM::forEach(sf,v);
+
+        BOOST_CHECK( sum == 28 + 242 + 28 );
+    }
+
+
+    { // vars, pack, seq
+        SA::clear(v);
+        int a,b,c,d,e,f,g;
+        auto p = SF::pack(a,b,c,d,e,f,g);
+        SM::distribute(SF::seqI(1,7),p);
+
+        SA::add(v,a,b,c,d,e,f,g,p,SF::seqI(17,27));
+
+        sum = 0;
+        SM::forEach(sf,v);
+
+        BOOST_CHECK( sum == 28 + 242 + 28 );
+    }
+
+    { // vars, seq, pack
+        SA::clear(v);
+        int a,b,c,d,e,f,g;
+        auto p = SF::pack(a,b,c,d,e,f,g);
+        SM::distribute(SF::seqI(1,7),p);
+
+        SA::add(v,a,b,c,d,e,f,g,SF::seqI(17,27),p);
+
+        sum = 0;
+        SM::forEach(sf,v);
+
+        BOOST_CHECK( sum == 28 + 242 + 28 );
+    }
+
+
+    { // seq, vars, pack
+        SA::clear(v);
+        int a,b,c,d,e,f,g;
+        auto p = SF::pack(a,b,c,d,e,f,g);
+        SM::distribute(SF::seqI(1,7),p);
+
+        SA::add(v,SF::seqI(17,27),a,b,c,d,e,f,g,p);
+
+        sum = 0;
+        SM::forEach(sf,v);
+
+        BOOST_CHECK( sum == 28 + 242 + 28 );
+    }
+
+    { // seq, pack, vars
+        SA::clear(v);
+        int a,b,c,d,e,f,g;
+        auto p = SF::pack(a,b,c,d,e,f,g);
+        SM::distribute(SF::seqI(1,7),p);
+
+        SA::add(v,SF::seqI(17,27),p,a,b,c,d,e,f,g);
+
+        sum = 0;
+        SM::forEach(sf,v);
+
+        BOOST_CHECK( sum == 28 + 242 + 28 );
+    }
+}
 
 BOOST_AUTO_TEST_SUITE_END();
 

64b3da7a53f9dea4c47e9e360e0344336aea9582
David <david@templatious.org>
2014-12-26 09:21:29 +0200

    new test suite

---
+++ StaticManipulatorTests.h
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  StaticManipulatorTests.h
+ *
+ *    Description:  Static manipulator tests
+ *
+ *        Version:  1.0
+ *        Created:  12/26/2014 09:20:37 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#ifndef STATICMANIPULATORTESTS_JT4V7DJV
+#define STATICMANIPULATORTESTS_JT4V7DJV
+
+#include "TestDefs.h"
+
+BOOST_AUTO_TEST_SUITE( static_manipulator_tests );
+
+BOOST_AUTO_TEST_SUITE_END();
+
+#endif /* end of include guard: STATICMANIPULATORTESTS_JT4V7DJV */

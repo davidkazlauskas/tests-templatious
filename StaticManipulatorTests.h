07f96b7093ab72969f2c4527f17cb695182b434e
David <david@templatious.org>
2015-01-29 19:27:07 +0200

    multi

--- StaticManipulatorTests.h
+++ StaticManipulatorTests.h
@@ -434,6 +434,15 @@ BOOST_AUTO_TEST_CASE( static_manipulator_collection_equality_basic )
     BOOST_CHECK( SM::areCollectionsEqual(v,s) );
 }
 
+BOOST_AUTO_TEST_CASE( static_manipulator_collection_equality_multi )
+{
+    std::vector<long> v;
+    auto s = SF::seqL(100);
+    SA::add(v,s);
+
+    BOOST_CHECK( SM::areCollectionsEqual(v,s,SF::seqL(100)) );
+}
+
 BOOST_AUTO_TEST_SUITE_END();
 
 #endif /* end of include guard: STATICMANIPULATORTESTS_JT4V7DJV */

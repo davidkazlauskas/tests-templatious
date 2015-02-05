dabe35ceefcacac4572bf142653eb084a73f5852
David <david@templatious.org>
2015-02-05 19:35:57 +0200

    new header

---
+++ StaticFactoryTests.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  StaticFactoryTests.h
+ *
+ *    Description:  Static factory tests
+ *
+ *        Version:  1.0
+ *        Created:  2015.02.05 19:35:02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#ifndef STATICFACTORYTESTS_ZJBL62XK
+#define STATICFACTORYTESTS_ZJBL62XK
+
+#include "TestDefs.h"
+
+BOOST_AUTO_TEST_SUITE( static_factory_tests );
+
+
+BOOST_AUTO_TEST_SUITE_END();
+
+#endif /* end of include guard: STATICFACTORYTESTS_ZJBL62XK */

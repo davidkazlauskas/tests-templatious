71f71e600e50d26cae71449b01ab65836d03dfbd
David <david@templatious.org>
2015-03-10 21:21:53 +0200

    dat whitespace

--- spec/StaticVector.cpp
+++ spec/StaticVector.cpp
@@ -22,7 +22,6 @@ static const int SUM_1_TO_256 = 32896;
 
 TEST_CASE( "static_vector_tests_basic", "[static_vector_tests]" )
 {
-
     const size_t SIZE = 256;
     tt::t::StaticBuffer<int,SIZE> b;
     auto v = b.getStaticVector();

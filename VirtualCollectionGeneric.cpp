85cd89ba5b329ad43e7d9a96383c036b3bbddde5
David <david@templatious.org>
2015-03-19 17:45:18 +0200

    carcass

---
+++ VirtualCollectionGeneric.cpp
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  VirtualCollectionGeneric.cpp
+ *
+ *    Description:  Generic applicable to all
+ *
+ *        Version:  1.0
+ *        Created:  2015.03.19 17:43:42
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Deividas Kazlauskas (dk), ne-bobu@reikalas.lt
+ *        Company:  Company and shit
+ *
+ * =====================================================================================
+ */
+
+#include "TestDefs.hpp"
+
+TEST_CASE( "virtual_collection_move_semantics", "[virtual_collection]" ) {
+
+}

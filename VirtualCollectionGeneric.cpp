cce0a952ae9a36887a29d39f04a101baca6d5713
David <david@templatious.org>
2015-03-19 17:51:09 +0200

    dat test

--- VirtualCollectionGeneric.cpp
+++ VirtualCollectionGeneric.cpp
@@ -18,6 +18,16 @@
 
 #include "TestDefs.hpp"
 
-TEST_CASE( "virtual_collection_move_semantics", "[virtual_collection]" ) {
+std::vector<int> rvalVect() {
+    std::vector<int> res;
+    SA::add(res,1,2,3,4,5,6,7);
+    return std::move(res);
+}
 
+TEST_CASE( "virtual_collection_move_semantics", "[virtual_collection]" ) {
+    auto vc = SF::vcollection(rvalVect());
+
+    int sum = 1 + 2 + 3 + 4 + 5 + 6 + 7;
+
+    REQUIRE( SM::sum<int>(vc) == sum );
 }

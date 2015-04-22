eccfddd0a30444f4a1dc1e64974ba17efe2d2c99
David <david@templatious.org>
2015-04-22 19:24:56 +0300

    unsynced

--- VirtualPackTests.cpp
+++ VirtualPackTests.cpp
@@ -820,3 +820,44 @@ TEST_CASE( "virtual_pack_custom_synchronized_with_callback", "[virtual_pack_test
     int outExpected = (ROUNDS * 2) * (1 + 2);
     REQUIRE( *iPtr == outExpected );
 }
+
+TEST_CASE( "virtual_pack_custom_unsynchronized_with_callback", "[virtual_pack_tests]" )
+{
+    // increment this from threads
+    auto iPtr = std::make_shared< int >( 0 );
+
+    auto pack = SF::vpackPtrCustomWCallback<
+        0,
+        int,int
+    >([=](const TEMPLATIOUS_VPCORE<int,int>& vp) {
+        *iPtr += vp.fGet<0>();
+        *iPtr += vp.fGet<1>();
+    },1,2);
+
+    const int ROUNDS = 100000;
+
+    // callbacks must also be synchronized
+    auto handleA = std::async(std::launch::async,
+        [=]() {
+            TEMPLATIOUS_REPEAT( ROUNDS ) {
+                pack->tryCallFunction<int,int>(
+                    [](int& a,int& b) {}
+                );
+            }
+        });
+
+    auto handleB = std::async(std::launch::async,
+        [=]() {
+            TEMPLATIOUS_REPEAT( ROUNDS ) {
+                pack->tryCallFunction<int,int>(
+                    [](int& a,int& b) {}
+                );
+            }
+        });
+
+    handleA.wait();
+    handleB.wait();
+
+    int outExpected = (ROUNDS * 2) * (1 + 2);
+    REQUIRE( *iPtr != outExpected );
+}

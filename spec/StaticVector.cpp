3c2c694031785dd4f2c99f47e82bd1206b9591c6
David <david@templatious.org>
2015-05-05 21:07:00 +0300

    more coverage

--- spec/StaticVector.cpp
+++ spec/StaticVector.cpp
@@ -743,5 +743,15 @@ TEST_CASE( "static_vector_erase_assertions", "[static_vector_tests]" )
         }
         REQUIRE( caught );
     }
+
+    {
+        bool caught = false;
+        try {
+            SA::erase(v1,SA::begin(v1),SA::end(v2));
+        } catch (const tt::t::StaticVectorEraseException& e) {
+            caught = true;
+        }
+        REQUIRE( caught );
+    }
 }
 

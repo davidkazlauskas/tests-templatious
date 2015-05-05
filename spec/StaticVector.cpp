12bce1ef0454e34c3a8ac1890dc562e24d3e09e8
David <david@templatious.org>
2015-05-05 21:11:44 +0300

    hail to the king of coverage

--- spec/StaticVector.cpp
+++ spec/StaticVector.cpp
@@ -773,5 +773,15 @@ TEST_CASE( "static_vector_erase_assertions", "[static_vector_tests]" )
         }
         REQUIRE( caught );
     }
+
+    {
+        bool caught = false;
+        try {
+            SA::erase(v1,SA::begin(v1),SA::end(v1) + 1);
+        } catch (const tt::t::StaticVectorEraseException& e) {
+            caught = true;
+        }
+        REQUIRE( caught );
+    }
 }
 

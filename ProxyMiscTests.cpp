389c8d1ba034a4178cdd97b0fd61a86736cab8d7
David <david@templatious.org>
2015-05-06 20:31:41 +0300

    moar coverage

--- ProxyMiscTests.cpp
+++ ProxyMiscTests.cpp
@@ -26,7 +26,22 @@ TEST_CASE( "proxy_range_iter_at_rand_access", "[proxy_tests]" )
 
     REQUIRE( *SA::iterAt(r,7) == 17 );
 
+    auto beg = SA::iterAt(r,1);
+    REQUIRE( *beg == 11 );
+    --beg;
+    REQUIRE( *beg == 10 );
+
     r.clear();
     REQUIRE( r.size() == 0 );
+}
+
+TEST_CASE( "proxy_range_iter_only_beg", "[proxy_tests]" )
+{
+    std::vector<int> v;
+    SA::add(v,SF::seqL(100));
+
+    auto r = SF::range(v,SA::iterAt(v,10));
+    REQUIRE( *SA::iterAt(r,0) == 10 );
+    REQUIRE( *SA::iterAt(r,89) == 99 );
 }
 
